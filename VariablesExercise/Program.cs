1f239dc5563993f44fd0b00e660a461e488692e9
Ashlyn Carmosino <ashlyncarmosino@Ashlyns-Air.lan>
2023-01-31 10:39:21 -0500

    Declarations and Initialization

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string Luna; // declaration 
+            Luna = "Luna Bear"; //initialization 
+
+            int LunaAge = 2; // declaring and initializing 
+
+            Char L = 'L'; 
+
+            bool Hungry = true;
+
+            double lunaWeight = 73.7;
+
+            decimal lunaHeight = 4.2m; //has to have lowwer case m
+
+            Console.WriteLine($"My dog's name is {Luna}, she is {LunaAge} years old");
+
+            Console.WriteLine($"Luna is {lunaWeight} lbs");
+
+
         }
     }
 }
